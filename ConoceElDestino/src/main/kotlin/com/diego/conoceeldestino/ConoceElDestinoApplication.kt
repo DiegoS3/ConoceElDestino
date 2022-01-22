2bf5ad4cfe04dd379b6763a2baa324457238b0a0
Diego Sánchez <diegozj92@gmail.com>
2022-01-22 12:14:27 +0100

    Created Project

---
+++ ConoceElDestino/src/main/kotlin/com/diego/conoceeldestino/ConoceElDestinoApplication.kt
@@ -0,0 +1,11 @@
+package com.diego.conoceeldestino
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConoceElDestinoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConoceElDestinoApplication>(*args)
+}
---
+++ ConoceElDestino/src/test/kotlin/com/diego/conoceeldestino/ConoceElDestinoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diego.conoceeldestino
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConoceElDestinoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
