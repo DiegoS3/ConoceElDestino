a56b07df9e568cf17a51acb03254c256fb11ecbe
Diego Sánchez <diegozj92@gmail.com>
2022-05-02 17:42:48 +0200

    repository

---
+++ ConoceElDestino/src/main/kotlin/com/diego/conoceeldestino/repository/ServiceRepository.kt
@@ -0,0 +1,8 @@
+package com.diego.conoceeldestino.repository;
+
+import com.diego.conoceeldestino.entity.Service
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ServiceRepository : CrudRepository<Service, Int> 
