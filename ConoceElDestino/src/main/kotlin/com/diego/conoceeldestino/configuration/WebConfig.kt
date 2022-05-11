c597ef0a2f99964f87b1b6cb2adbd87190844d50
Diego Sánchez <diegozj92@gmail.com>
2022-05-11 14:40:51 +0200

    name app

---
+++ ConoceElDestino/src/main/kotlin/com/diego/conoceeldestino/configuration/WebConfig.kt
@@ -0,0 +1,29 @@
+package com.diego.conoceeldestino.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("https://https://conoceeldestinofront-app.herokuapp.com")
+            .allowedMethods("*")
+            .maxAge(3600L)
+            .allowedHeaders("*")
+            .exposedHeaders("Authorization")
+            .allowCredentials(true)
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+}
